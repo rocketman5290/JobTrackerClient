c0626f885a509575c86fff2f60cb1685e2570179
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-10-20 20:50:48 -0400

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
-import { JobsList, NewJobForm, Divider } from './components/Jobs';
+import { JobsList, NewJobForm } from './components/Jobs';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        axios.get('api/v1/jobs.json')
+        .then(res => setJobs(res.data))
+    }, []);
 
   return (
     <div className="App">
         <h1>Jobs</h1>
-        <JobsList />
-        <Divider />
-        <NewJobForm />
+        <JobsList jobs={jobs} setJobs={setJobs}/>
+        <br></br>
+        <NewJobForm jobs={jobs} setJobs={setJobs}/>
     </div>
   );
 }
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,36 +1,9 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 import qs from 'qs'
 
-const Divider = () => {
-    return(
-        <div className="divider">
-            <br>
-            </br>
-        </div>
-    )
-}
-
-// TODO: turn to hook properly
-const useAddJob = job => {
-        console.log(job)
-        
-        axios.post('/api/v1/jobs', qs.stringify(
-            {
-              job:{
-                company: job.company,
-                position: job.position,
-                description: job.description}
-            }))
-            .then(res=>( console.log(res)))
-            .catch( error => console.log(error))
-}
-
-const NewJobForm = () => {
-    const [formState, updateFormState] = useState({ company:'',
-                                                    position:'',
-                                                    description:''
-})
+const NewJobForm = props => {
+    const [formState] = useState({ company:'', position:'', description:''})
 
     const [job, setJob ] = useState(formState)
 
@@ -43,50 +16,73 @@ const NewJobForm = () => {
                     position: job.position,
                     description: job.description}
                 }))
-                .then(res => (console.log(res)))
+                .then(res => {
+                    console.log(res.data)
+                    return props.setJobs([...props.jobs, job])
+                })
                 .catch( error => console.log(error))
             )
     }
     
     const handleInputChange = event => {
         const {name, value} = event.target
+        console.log(name, value)
         setJob({...job, [name]: value})
     }
 
     return (
-        <form onSubmit={event => {
-          event.preventDefault()
-          if (!job.company || !job.position || !job.description) return;
-          setJob(formState)
-          addJob(job)
-        }}>
-          <label>Company</label>
-          <input type="text" name="company" value={job.company} onChange={handleInputChange} ></input>
-          <label>Position</label>
-          <input type="text" name="position" value={job.position} onChange={handleInputChange} ></input>
-          <label>Description</label>
-          <input type="text" name="description" value={job.description} onChange={handleInputChange} ></input>
-          <button>Create Job</button>
-        </form>
+        <div className="addAndDeleteJobForm">
+            <div>
+                <form onSubmit={event => {
+                    event.preventDefault()
+                    if (!job.company || !job.position || !job.description) return;
+                        setJob(formState)
+                        addJob(job)
+                }}>
+                <label>Company</label>
+                <input type="text" name="company" value={job.company} onChange={handleInputChange}></input>
+                <label>Position</label>
+                <input type="text" name="position" value={job.position} onChange={handleInputChange} ></input>
+                <label>Description</label>
+                <input type="text" name="description" value={job.description} onChange={handleInputChange} ></input>
+                <button>Create Job</button>
+                </form>
+            </div>
+        </div>
+
     )
-}
+}; //end of jobFormComponent
 
-const JobsList = () => {
-    useEffect(() => {
-        axios.get('api/v1/jobs.json')
-        .then(res => setJobs(res.data))
-    }, []);
+const JobsList = props => {
+    // const [jobs, setJobs] = useState([]);
 
-    const [jobs, setJobs] = useState([]);
+    // useEffect(() => {
+    //     axios.get('api/v1/jobs.json')
+    //     .then(res => setJobs(res.data))
+    // }, []);
+
+    const handleJobDelete = (_event, id) => {
+        axios.delete("/api/v1/jobs/" + String(id))
+            .then(res => props.setJobs(props.jobs.filter(job => job.id !== id )))
+            .catch(error => console.log(error))
+    }
 
     return(
         <div className='jobs-list'>
-            { jobs.map( (job, index) => (
-                 <div key={index}>
-                    {job.company} | {job.position} | {job.description}
+            { props.jobs.map( (job, index) => (
+                <div key={index}>
+                    <div className="jobDetails">
+                        {job.company} | {job.position} | {job.description} 
+                     </div>
+                    <div className="deleteJobButton">
+                        <button onClick={(event) => handleJobDelete(event, job.id)}>
+                            Delete Job
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
     )
-};
-export {JobsList, NewJobForm, Divider};
+}; //end of JobListComponent
+
+export {JobsList, NewJobForm};
